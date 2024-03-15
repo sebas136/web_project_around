e0c5046744709938e7b5897ceca6541af2b953ea
sebastian007 <sebas136@gmail.com>
2024-03-15 18:40:46 -0500

    Actualizacions

--- scripts/script.js
+++ scripts/script.js
@@ -4,37 +4,41 @@ const editButton = main.querySelector(".profile__edit-button");
 const editProfile = main.querySelector(".popup");
 const closeButton = editProfile.querySelector(".popup__close");
 
-function popupToggle() {
+function popupProfileToggle() {
   editProfile.classList.toggle("popup__opened");
   inputName.value = profileName.textContent;
   inputDescription.value = profileAbout.textContent;
-  submitButton.disabled = true;
+  
 }
 
-closeButton.addEventListener("click", popupToggle);
-editButton.addEventListener("click", popupToggle);
+function popupToggleNewCard() {
+
+}
+
+closeButton.addEventListener("click", popupProfileToggle);
+editButton.addEventListener("click", popupProfileToggle);
 
 const profileName = main.querySelector(".profile__name");
 const profileAbout = main.querySelector(".profile__about");
 const inputName = editProfile.querySelector(".popup__input_name");
 const inputDescription = editProfile.querySelector(".popup__input_about");
 const submitButton = editProfile.querySelector(".popup__submit");
+const formProfile = editProfile.querySelector('form');
 
-function changeForm() {
+function changeForm(event) {
+  event.preventDefault();
   const nameValue = inputName.value;
   const descriptionValue = inputDescription.value;
   const profileNameText = profileName.textContent;
   const profileAboutText = profileAbout.textContent;
-  const isAnyInputEmpty = nameValue === "" || descriptionValue === "";
-  const hasChanged = nameValue !== profileNameText || descriptionValue !== profileAboutText;
-  submitButton.disabled = !(hasChanged && !isAnyInputEmpty);
+  
 }
 
-editProfile.addEventListener("input", changeForm);
+formProfile.addEventListener("submit", changeForm);
 
 submitButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputDescription.value;
-  popupToggle();
+  popupProfileToggle();
 });
