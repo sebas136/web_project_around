5e83413118a06adc9097fe054a6f4700aa41714a
sebastian007 <sebas136@gmail.com>
2024-03-18 12:49:34 -0500

    Actualizacions

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,71 @@
-// elemen-elemen 
-const editButton = document.querySelector(".profile__edit-button");
-const editProfilePopup = document.querySelector(".popup");
-const closePopupButton = editProfilePopup.querySelector(".popup__close");
-const nameInput = editProfilePopup.querySelector(".popup__input_name");
-const aboutInput = editProfilePopup.querySelector(".popup__input_about");
-const profilName = document.querySelector(".profile__name");
-const profileAbout = document.querySelector(".profile__about");
-const editProfileForm = document.querySelector(".popup__container");
+const popUpProfile = document.querySelector("#popup-profile");
+const btnEdit = document.querySelector(".profile__edit");
+const btnCloseProfile = document.querySelector("#close-profile");
+const profileNameInput = document.querySelector("#input-name");
+const profileAboutInput = document.querySelector("#input-title");
+const profileName = document.querySelector(".profile__info");
+const profileAbout = document.querySelector(".profile__title");
+const btnProfileSave = document.querySelector(".popup__button-save");
+const formProfile = document.querySelector(".popup__form");
+const popUpAdd = document.querySelector("#popup-add");
+const btnAdd = document.querySelector(".profile__add");
+const btnCloseAdd = document.querySelector("#close-add");
 
-//  data profil 
-function openEditPopup() {
-  nameInput.value = profilName.textContent;
-  aboutInput.value = profileAbout.textContent;
-  editProfilePopup.classList.add("popup_opened");
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
+
+//open close profile profile
+function openProfile() {
+  popUpProfile.classList.toggle("popup__show");
+  profileNameInput.value = profileName.textContent;
+  profileAboutInput.value = profileAbout.textContent;
+}
+function closeProfile() {
+  popUpProfile.classList.remove("popup__show");
 }
 
-// popup
-function closeEditPopup() {
-  editProfilePopup.classList.remove("popup_opened");
-}
-
-//  "Edit Profil"
-editButton.addEventListener("click", openEditPopup);
-
-// "Pop"
-closePopupButton.addEventListener("click", closeEditPopup);
-
-editProfileForm.addEventListener("submit", function (evt) {
+//Editar profile
+function saveChanges(evt) {
   evt.preventDefault();
-  profilName.textContent = nameInput.value;
-  profileAbout.textContent = aboutInput.value;
-  closeEditPopup();
-});
+  profileName.textContent = profileNameInput.value;
+  profileAbout.textContent = profileAboutInput.value;
+  closeProfile();
+}
+
+btnEdit.addEventListener("click", openProfile);
+btnCloseProfile.addEventListener("click", closeProfile);
+formProfile.addEventListener("submit", saveChanges);
+
+//open close add
+function openAdd() {
+  popUpAdd.classList.toggle("popup__show");
+}
+function closeAdd() {
+  popUpAdd.classList.remove("popup__show");
+}
+btnAdd.addEventListener("click", openAdd);
+btnCloseAdd.addEventListener("click", closeAdd);
